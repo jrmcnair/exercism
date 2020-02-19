01cb837d7f13ab433a89df366436b20fa7b60f80
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2020-02-19 13:53:33 -0500

    My Solution, with help from the interwebs

--- fsharp/hamming/Hamming.fs
+++ fsharp/hamming/Hamming.fs
@@ -1,3 +1,15 @@
 ﻿module Hamming
 
-let distance (strand1: string) (strand2: string): int option = failwith "You need to implement this function."
+open System
+
+let private hamm (strand1: string) (strand2: string): int =
+    Seq.map2(fun x y ->
+        match x = y with
+        | true -> 0
+        | false -> 1
+    ) strand1 strand2 |> Seq.sum
+
+let distance (strand1: string) (strand2: string): int option =
+    match (strand1, strand2) with
+    | (s1, s2) when not (s1.Length = s2.Length) -> None
+    | (s1, s2) -> hamm s1 s2 |> Some
