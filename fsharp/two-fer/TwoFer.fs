72b174ca683e56b4e81a15e169dc669f1cf8cd68
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2020-02-19 15:25:05 -0500

    attempt #2, after feedback from mentor

--- fsharp/two-fer/TwoFer.fs
+++ fsharp/two-fer/TwoFer.fs
@@ -1,9 +1,6 @@
 ﻿module TwoFer
 
-let private msg (name: string): string =
-    sprintf "One for %s, one for me." name
-
 let twoFer (input: string option): string =
-    match input with
-    | None | Some "" -> msg "you"
-    | Some name -> msg name
+    input
+    |> Option.defaultValue "you"
+    |> sprintf "One for %s, one for me."
