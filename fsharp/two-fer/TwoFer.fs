47ca6487d596c5d4967ee0904547d22e7a85e88a
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2020-02-19 14:04:12 -0500

    My solution for two-fer

---
+++ fsharp/two-fer/TwoFer.fs
@@ -0,0 +1,9 @@
+﻿module TwoFer
+
+let private msg (name: string): string =
+    sprintf "One for %s, one for me." name
+
+let twoFer (input: string option): string =
+    match input with
+    | None | Some "" -> msg "you"
+    | Some name -> msg name
