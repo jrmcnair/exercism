363d5ea96c6b19fe5b36ffa528fb982f86338159
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2020-02-20 10:47:09 -0500

    fsharp: queen attack approved solution

---
+++ fsharp/queen-attack/QueenAttack.fs
@@ -0,0 +1,23 @@
+﻿module QueenAttack
+
+open System
+
+let create (position: int * int) =
+    let x, y = position
+    x >= 0 && x <= 7 && y >= 0 && y <= 7
+
+let canAttack (queen1: int * int) (queen2: int * int) =
+    let x1, y1 = queen1
+    let x2, y2 = queen2
+
+    let horizontalAttack =
+        x1 = x2
+
+    let verticalAttack =
+        y1 = y2
+
+    let diagonalAttack =
+        abs (y2 - y1) = abs (x2 - x1)
+
+    create queen1 && create queen2
+    && (horizontalAttack || verticalAttack || diagonalAttack)
