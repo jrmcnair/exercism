6d293bfed0dfb207a36b89d59cb6c014a8bd4c38
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2020-02-20 14:41:26 -0500

    fsharp: raindrops initial solution

---
+++ fsharp/raindrops/Raindrops.fs
@@ -0,0 +1,22 @@
+﻿module Raindrops
+
+let isDivisibleBy (divisor: int) (number: int) =
+    number % divisor = 0
+
+let sounds = [
+    (3, "Pling");
+    (5, "Plang");
+    (7, "Plong");
+]
+
+let convert (number: int): string =
+    let result =
+        List.fold (fun state (divisor,sound) ->
+            match isDivisibleBy divisor number with
+            | true -> sprintf "%s%s" state sound
+            | false -> state
+        ) "" sounds
+
+    match result with
+    | "" -> string number
+    | _ -> result
