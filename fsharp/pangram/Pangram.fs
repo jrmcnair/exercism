3528d1829d76ed18f1180ed1db4ba6a5939f251e
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2021-08-11 19:44:44 -0400

    [fsharp] pangram: final solution

---
+++ fsharp/pangram/Pangram.fs
@@ -0,0 +1,10 @@
+﻿module Pangram
+
+let alphabet = set ['a'..'z']
+
+let isPangram (input: string): bool =
+    match input with
+    | input when isNull input || input.Length < 26 -> false
+    | input ->
+        set (input.ToLowerInvariant())
+        |> Set.isSubset alphabet
