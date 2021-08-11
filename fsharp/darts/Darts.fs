64e1411d18965adc45ca6730b4dcf4694e570c29
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2021-08-11 19:54:47 -0400

    [fsharp] darts: final solution

---
+++ fsharp/darts/Darts.fs
@@ -0,0 +1,18 @@
+﻿module Darts
+
+let (|Outside|Outer|Middle|Inner|) (distance: double) =
+    match distance with
+    | d when d <= 1. -> Inner
+    | d when d <= 5. -> Middle
+    | d when d <= 10. -> Outer
+    | _ -> Outside
+
+let distance (x: float, y:float) =
+    (x*x + y*y) |> sqrt
+
+let score (x: double) (y: double): int =
+    match distance (x, y) with
+    | Inner -> 10
+    | Middle -> 5
+    | Outer -> 1
+    | Outside -> 0
