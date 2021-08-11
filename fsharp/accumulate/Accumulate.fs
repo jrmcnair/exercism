11328e90a33761244bd11c015f709e4a09717481
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2021-08-11 18:40:52 -0400

    [fsharp] accumulate: final solution

---
+++ fsharp/accumulate/Accumulate.fs
@@ -0,0 +1,15 @@
+﻿module Accumulate
+
+let accumulate (func: 'a -> 'b) (input: 'a list): 'b list =
+    //input |> List.map func
+
+    let rec tailRecursion (f: 'a -> 'b) (acc: 'b list) = function
+        | [] -> acc
+        | headItem::remainingItems ->
+            let convertedHeadItem = f headItem
+            let updatedAcc = convertedHeadItem :: acc
+
+            tailRecursion f updatedAcc remainingItems
+        
+    tailRecursion func [] input
+    |> List.rev
