db1ce1329ef09f57b44731c14e8f736133d4ebf9
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2021-08-12 08:39:32 -0400

    [fsharp] grade-school: submitted solution

--- fsharp/grade-school/GradeSchool.fs
+++ fsharp/grade-school/GradeSchool.fs
@@ -1,11 +1,34 @@
-﻿module GradeSchool
+module GradeSchool
 
 type School = Map<int, string list>
 
-let empty: School = failwith "You need to implement this function."
+let empty: School = Map.empty<int, string list>
 
-let add (student: string) (grade: int) (school: School): School = failwith "You need to implement this function."
+let add (student: string) (grade: int) (school: School): School =
+    match Map.tryFind grade school with
+    | Some students ->
+        school
+        |> Map.remove grade
+        |> Map.add grade (List.append students [ student ])
 
-let roster (school: School): string list = failwith "You need to implement this function."
+        // Using Map.remove/add seems cleaner to me than Map.change
+        // both pass the tests, but I'm not sure which is considered best practice
 
-let grade (number: int) (school: School): string list = failwith "You need to implement this function."
+        // school
+        // |> Map.change grade ( fun existing ->
+        //     match existing with
+        //     | Some students -> List.append students [ student ] |> Some
+        //     | None -> [ student ] |> Some )
+
+    | None -> school |> Map.add grade [ student ]
+
+let roster (school: School) =
+    let addStudentsToRoster (roster: string list) (_: int) (students: string list) =
+        List.append roster (List.sort students)
+
+    school |> Map.fold addStudentsToRoster []
+
+let grade (number: int) (school: School) =
+    match Map.tryFind number school with
+    | Some students -> List.sort students
+    | None -> []
