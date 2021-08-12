a61c3e6a9dda452a6bd8ba53295d14878af7c29d
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2021-08-12 12:09:09 -0400

    [fsharp] grade-school: final solution with mentor tips addressed

--- fsharp/grade-school/GradeSchool.fs
+++ fsharp/grade-school/GradeSchool.fs
@@ -2,24 +2,11 @@ module GradeSchool
 
 type School = Map<int, string list>
 
-let empty: School = Map.empty<int, string list>
+let empty: School = Map.empty
 
 let add (student: string) (grade: int) (school: School): School =
     match Map.tryFind grade school with
-    | Some students ->
-        school
-        |> Map.remove grade
-        |> Map.add grade (List.append students [ student ])
-
-        // Using Map.remove/add seems cleaner to me than Map.change
-        // both pass the tests, but I'm not sure which is considered best practice
-
-        // school
-        // |> Map.change grade ( fun existing ->
-        //     match existing with
-        //     | Some students -> List.append students [ student ] |> Some
-        //     | None -> [ student ] |> Some )
-
+    | Some students -> school |> Map.add grade (List.append students [ student ])
     | None -> school |> Map.add grade [ student ]
 
 let roster (school: School) =
