255883b748e45725fd449b42f640825ce707e6e9
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2021-08-12 13:44:48 -0400

    [fsharp] clock: submitted solution

--- fsharp/clock/Clock.fs
+++ fsharp/clock/Clock.fs
@@ -1,9 +1,35 @@
 module Clock
 
-let create hours minutes = failwith "You need to implement this function."
+type Clock =
+  { Hours: int
+    Minutes: int }
 
-let add minutes clock = failwith "You need to implement this function."
+let processMinutes (minutes: int) : (int * int) =
+    let minute = minutes % 60
+    let hourOffset = minutes / 60
 
-let subtract minutes clock = failwith "You need to implement this function."
+    match minute with
+    | x when x >= 0 -> (hourOffset, minute)
+    | _ -> (hourOffset - 1, 60 + minute)
 
-let display clock = failwith "You need to implement this function."
+let processHours (hours: int) : int =
+    match hours % 24 with
+    | hour when hour >= 0 -> hour
+    | hour -> 24 + hour
+
+let create (hours:int) (minutes: int) =
+    let (hourOffset, minute) = processMinutes minutes
+    let hour = processHours (hours + hourOffset)
+
+    { Hours = hour; Minutes = minute }
+
+let add minutes clock =
+   create clock.Hours (clock.Minutes + minutes)
+
+let subtract minutes clock =
+   create clock.Hours (clock.Minutes - minutes)
+
+let display clock =
+    let to24Hour (num: int) = num.ToString("D2")
+
+    sprintf "%s:%s" (to24Hour clock.Hours) (to24Hour clock.Minutes)
