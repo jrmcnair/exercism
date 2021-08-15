0318cac50ac2d4d690c0e024d603150ab906dab4
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2021-08-15 19:42:15 -0400

    [fsharp] kindergarten-garden: submitted solution

--- fsharp/kindergarten-garden/KindergartenGarden.fs
+++ fsharp/kindergarten-garden/KindergartenGarden.fs
@@ -1,5 +1,30 @@
 ﻿module KindergartenGarden
 
-// TODO: define the Plant type
+type Plant =
+    | Clover
+    | Grass
+    | Radishes
+    | Violets
+    | Unknown
 
-let plants diagram student = failwith "You need to implement this function."
+let students =
+    [ "Alice"; "Bob"; "Charlie"; "David"; "Eve"; "Fred";
+      "Ginny"; "Harriet"; "Ileana"; "Joseph"; "Kincaid"; "Larry" ]
+
+let charToPlant = function
+    | 'C' -> Clover
+    | 'G' -> Grass
+    | 'R' -> Radishes
+    | 'V' -> Violets
+    | _ -> Unknown
+
+let plants (diagram: string) (student: string) =
+    let rows = diagram.Split("\n")
+    let gardenPosition = (students |> List.findIndex (fun x -> x = student)) * 2
+
+    [
+        rows.[0].[gardenPosition]
+        rows.[0].[gardenPosition + 1]
+        rows.[1].[gardenPosition]
+        rows.[1].[gardenPosition + 1]
+    ] |> List.map charToPlant
