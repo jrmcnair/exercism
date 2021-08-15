5e545c9f2c52899eeb571b50f6dc37532b15ca4c
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2021-08-15 12:32:37 -0400

    [fsharp] beer-song: resubmission after initial mentor feedback

--- fsharp/beer-song/BeerSong.fs
+++ fsharp/beer-song/BeerSong.fs
@@ -1,27 +1,26 @@
 ﻿module BeerSong
 
-let private bottles (number: int) =
-    // could handle 0 here too, but I thought the init cap handling made it much less clean
-    if number = 1 then "1 bottle"
-    else sprintf "%d bottles" number
-
 let private verse (number: int) =
-    [
-        match number with 
-        | 0 -> "No more bottles of beer on the wall, no more bottles of beer."
-        | _ -> System.String.Format("{0} of beer on the wall, {0} of beer.", (bottles number))
-
-        match number with
-        | 0 -> "Go to the store and buy some more, 99 bottles of beer on the wall."
-        | 1 -> "Take it down and pass it around, no more bottles of beer on the wall."
-        | _ -> sprintf "Take one down and pass it around, %s of beer on the wall." (bottles (number - 1))
-    ]
+    match number with
+    | 0 ->
+        ("No more bottles of beer on the wall, no more bottles of beer.",
+         "Go to the store and buy some more, 99 bottles of beer on the wall.")
+    | 1 ->
+        ("1 bottle of beer on the wall, 1 bottle of beer.",
+         "Take it down and pass it around, no more bottles of beer on the wall.")
+    | 2 ->
+        ("2 bottles of beer on the wall, 2 bottles of beer.",
+         "Take one down and pass it around, 1 bottle of beer on the wall.")
+    | _ ->
+        ($"{number} bottles of beer on the wall, {number} bottles of beer.",
+         $"Take one down and pass it around, {number - 1} bottles of beer on the wall.")
 
 let recite (firstBottle: int) (takeDown: int) =
     let lastBottle = firstBottle - takeDown + 1
 
-    [firstBottle .. -1 .. lastBottle]
-    |> List.fold ( fun acc bottle ->
-            let spacer = if not (List.isEmpty acc) then [ "" ] else []
-            verse bottle |> List.append spacer |> List.append acc
-        ) []
+    [lastBottle .. firstBottle]
+    |> List.fold (fun acc bottle ->
+        let (line1, line2) = verse bottle
+        let acc = if List.isEmpty acc then acc else "" :: acc
+        line1 :: line2 :: acc
+    ) []
