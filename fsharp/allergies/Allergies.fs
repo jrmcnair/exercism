2be62c6701a83832f140b32ce0149a9466970e6b
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2021-08-16 17:18:57 -0400

    [fsharp] allergies: submitted solution

--- fsharp/allergies/Allergies.fs
+++ fsharp/allergies/Allergies.fs
@@ -1,9 +1,30 @@
 ﻿module Allergies
 
-open System
+type Allergen =
+    | Eggs
+    | Peanuts
+    | Shellfish
+    | Strawberries
+    | Tomatoes
+    | Chocolate
+    | Pollen
+    | Cats
 
-// TODO: define the Allergen type
+let private allergenCodes =
+    [ (Eggs, 1); (Peanuts, 2); (Shellfish, 4); (Strawberries, 8);
+      (Tomatoes, 16); (Chocolate, 32); (Pollen, 64); (Cats, 128) ]
+    |> Map.ofList
 
-let allergicTo codedAllergies allergen = failwith "You need to implement this function."
+let private allergenCodeFound codedAllergies allergenCode =
+    (codedAllergies &&& allergenCode) = allergenCode
 
-let list codedAllergies = failwith "You need to implement this function."
+let allergicTo codedAllergies allergen =
+    allergenCodes.[allergen] |> allergenCodeFound codedAllergies
+
+let list (codedAllergies: int) : Allergen list =
+    allergenCodes
+    |> Map.toList
+    |> List.choose ( fun (allergen, allergenCode) ->
+        match allergenCodeFound codedAllergies allergenCode with
+        | true -> Some allergen
+        | _ -> None )
