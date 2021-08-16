fcc3a4e763bc75e928246bd7c90e6e3144645cf8
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2021-08-16 08:14:08 -0400

    [fsharp] robot-simulator: submitted solution

--- fsharp/robot-simulator/RobotSimulator.fs
+++ fsharp/robot-simulator/RobotSimulator.fs
@@ -4,6 +4,34 @@ type Direction = North | East | South | West
 type Position = int * int
 type Robot = { direction: Direction; position: Position }
 
-let create direction position = failwith "You need to implement this function."
+let create direction position =
+    { direction = direction; position = position }
 
-let move instructions robot = failwith "You need to implement this function."
+let rightTurn = function
+    | North -> East
+    | East -> South
+    | South -> West
+    | West -> North
+
+let leftTurn = function
+    | North -> West
+    | East -> North
+    | South -> East
+    | West -> South
+
+let advance (x:int, y:int) = function
+    | North -> (x, y + 1)
+    | East -> (x + 1, y)
+    | South -> (x, y - 1)
+    | West -> (x - 1 , y)
+
+let processInstruction robot = function
+    | 'R' -> { robot with direction = rightTurn robot.direction }
+    | 'L' -> { robot with direction = leftTurn robot.direction }
+    | 'A' -> { robot with position = advance robot.position robot.direction }
+    | _ -> robot
+
+let move instructions robot =
+    instructions
+    |> Seq.fold(fun state instruction -> processInstruction state instruction) robot
+
