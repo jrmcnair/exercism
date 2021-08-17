e5c6fc3a785c26026792f3e363dc75ddc0e9f896
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2021-08-16 21:34:12 -0400

    [fsharp] allergies: solution with suggestions from mentor

--- fsharp/allergies/Allergies.fs
+++ fsharp/allergies/Allergies.fs
@@ -1,30 +1,21 @@
 ﻿module Allergies
 
+open System
+
 type Allergen =
-    | Eggs
-    | Peanuts
-    | Shellfish
-    | Strawberries
-    | Tomatoes
-    | Chocolate
-    | Pollen
-    | Cats
-
-let private allergenCodes =
-    [ (Eggs, 1); (Peanuts, 2); (Shellfish, 4); (Strawberries, 8);
-      (Tomatoes, 16); (Chocolate, 32); (Pollen, 64); (Cats, 128) ]
-    |> Map.ofList
-
-let private allergenCodeFound codedAllergies allergenCode =
-    (codedAllergies &&& allergenCode) = allergenCode
+    | Eggs = 1
+    | Peanuts = 2
+    | Shellfish = 4
+    | Strawberries = 8
+    | Tomatoes = 16
+    | Chocolate = 32
+    | Pollen = 64
+    | Cats = 128
 
 let allergicTo codedAllergies allergen =
-    allergenCodes.[allergen] |> allergenCodeFound codedAllergies
+    (codedAllergies &&& (int) allergen) > 0
 
 let list (codedAllergies: int) : Allergen list =
-    allergenCodes
-    |> Map.toList
-    |> List.choose ( fun (allergen, allergenCode) ->
-        match allergenCodeFound codedAllergies allergenCode with
-        | true -> Some allergen
-        | _ -> None )
+    Enum.GetValues<Allergen>()
+    |> Seq.filter (allergicTo codedAllergies)
+    |> Seq.toList
