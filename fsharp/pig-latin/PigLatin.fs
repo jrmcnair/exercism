c3d755ca0750e462c94556b9270a0ba4260f3478
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2021-08-24 07:57:05 -0400

    [fsharp] pig-latin: submitted solution

--- fsharp/pig-latin/PigLatin.fs
+++ fsharp/pig-latin/PigLatin.fs
@@ -1,3 +1,25 @@
 ﻿module PigLatin
 
-let translate input = failwith "You need to implement this function."
+open System.Text.RegularExpressions
+
+let (|Vowel|_|) word =
+    match Regex.Match(word, "^([aeiou]|xr|yt)").Success with
+    | true -> Some word
+    | false -> None
+
+let (|Consonant|_|) word =
+    let result = Regex.Match(word, "^([^aeiou]*qu|[^aeiou][^aeiouy]+|[^aeiou]+)(.+)")
+    match result.Success with
+    | true -> Some (result.Groups.[1], result.Groups.[2])
+    | false -> None
+
+let translateWord (word: string) =
+    match word with
+    | Vowel word -> $"{word}ay"
+    | Consonant (consonant, remainder) -> $"{remainder}{consonant}ay"
+    | _ -> failwith $"unable to translate '{word}'"
+
+let translate (input: string) =
+    input.Split(' ') 
+    |> Seq.map translateWord
+    |> String.concat " "
