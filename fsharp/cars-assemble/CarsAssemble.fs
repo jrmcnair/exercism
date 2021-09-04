4258e6ed1596ef8d58cac0adcf3385830cb9e43d
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2021-09-03 21:56:25 -0400

    [fsharp] cars-assemble: final submission

--- fsharp/cars-assemble/CarsAssemble.fs
+++ fsharp/cars-assemble/CarsAssemble.fs
@@ -1,7 +1,16 @@
 module CarsAssemble
 
+let successRate = function
+    | 1 | 2 | 3 | 4 -> 1.
+    | 5 | 6 | 7 | 8 -> 0.9
+    | 9 -> 0.8
+    | 10 -> 0.77
+    | _ -> 0.
+
 let productionRatePerHour (speed: int): float =
-    failwith "Please implement the 'productionRatePerHour' function"
+    (float speed) * 221. * successRate speed
 
 let workingItemsPerMinute(speed: int): int =
-   failwith "Please implement the 'workingItemsPerMinute' function"
+    productionRatePerHour speed / 60.
+    |> floor
+    |> int
