c08a002f535bb16b73fa8f0c806af5b943569886
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2021-09-05 09:22:21 -0400

    [fsharp] bird-watcher: re-submission after viewing community entries

--- fsharp/bird-watcher/BirdWatcher.fs
+++ fsharp/bird-watcher/BirdWatcher.fs
@@ -5,13 +5,14 @@ let lastWeek: int[] = [| 0; 2; 5; 3; 7; 8; 4 |]
 let yesterday(counts: int[]): int = counts.[5]
 
 let total(counts: int[]): int =
-  counts |> Array.reduce (+)
+  counts |> Array.sum
 
 let dayWithoutBirds(counts: int[]): bool =
-  counts |> Array.exists (fun x -> x = 0)
+  counts |> Array.exists ((=) 0)
 
 let incrementTodaysCount(counts: int[]): int[] =
-    Array.append counts.[0..5] [| counts.[6] + 1 |]
+    counts.[6] <- counts.[6] + 1
+    counts
 
 let oddWeek(counts: int[]): bool =
   counts = [| 1; 0; 1; 0; 1; 0; 1 |]
