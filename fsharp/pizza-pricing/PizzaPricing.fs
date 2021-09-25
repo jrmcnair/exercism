e5ffdd69b85c6c8caee7e8bfb645a4f743a67700
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2021-09-25 17:17:03 -0400

    [fsharp] pizza-pricing: first submission

--- fsharp/pizza-pricing/PizzaPricing.fs
+++ fsharp/pizza-pricing/PizzaPricing.fs
@@ -1,7 +1,29 @@
 module PizzaPricing
 
-// TODO: please define the 'Pizza' discriminated union type
+type Pizza =
+    | Margherita
+    | Caprese
+    | Formaggio
+    | ExtraSauce of Pizza
+    | ExtraToppings of Pizza
 
-let pizzaPrice (pizza: Pizza): int = failwith "Please implement the 'pizzaPrice' function"
+let rec pizzaPrice = function
+    | Margherita -> 7
+    | Caprese -> 9
+    | Formaggio -> 10
+    | ExtraSauce x -> 1 + pizzaPrice x
+    | ExtraToppings x -> 2 + pizzaPrice x
 
-let orderPrice(pizzas: Pizza list): int = failwith "Please implement the 'orderPrice' function"
+let calculateFee = function
+    | 1 -> 3
+    | 2 -> 2
+    | _ -> 0
+
+let orderPrice(pizzas: Pizza list): int =
+    let fee = pizzas.Length |> calculateFee
+    let cost = 
+        pizzas
+        |> List.map pizzaPrice
+        |> List.fold (+) 0
+
+    cost + fee
