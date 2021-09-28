dbc79343e5b2039d141adb0779b949db43abc038
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2021-09-27 21:52:48 -0400

    [fsharp] twelve-days: second submission

--- fsharp/twelve-days/TwelveDays.fs
+++ fsharp/twelve-days/TwelveDays.fs
@@ -5,27 +5,23 @@ let days = [ "first"; "second"; "third"; "fourth"; "fifth"; "sixth"
 
 let gifts = [
     "a Partridge in a Pear Tree"
-    "two Turtle Doves"
-    "three French Hens"
-    "four Calling Birds"
-    "five Gold Rings"
-    "six Geese-a-Laying"
-    "seven Swans-a-Swimming"
-    "eight Maids-a-Milking"
-    "nine Ladies Dancing"
-    "ten Lords-a-Leaping"
-    "eleven Pipers Piping"
-    "twelve Drummers Drumming"
+    "two Turtle Doves, and "
+    "three French Hens, "
+    "four Calling Birds, "
+    "five Gold Rings, "
+    "six Geese-a-Laying, "
+    "seven Swans-a-Swimming, "
+    "eight Maids-a-Milking, "
+    "nine Ladies Dancing, "
+    "ten Lords-a-Leaping, "
+    "eleven Pipers Piping, "
+    "twelve Drummers Drumming, "
 ]
 
 let line idx =
-    match idx with
-    | 1 -> $"{gifts.[0]}"
-    | idx -> 
-        [1 .. idx-1]
-        |> List.map (fun x -> gifts.[x])
-        |> List.reduce (fun acc gift -> $"{gift}, {acc}")
-        |> (fun x -> $"{x}, and {gifts.[0]}")
+    [idx-1 .. -1 .. 0]
+    |> List.map (fun x -> gifts.[x])
+    |> List.reduce (+)
     |> sprintf "On the %s day of Christmas my true love gave to me: %s." days.[idx-1]
 
 let recite start stop =
