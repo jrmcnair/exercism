a0fb19734252fbfee0b714737b98d008c3b262c9
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2021-09-27 21:39:36 -0400

    [fsharp] twelve-days: first submission

--- fsharp/twelve-days/TwelveDays.fs
+++ fsharp/twelve-days/TwelveDays.fs
@@ -1,3 +1,33 @@
 ﻿module TwelveDays
 
-let recite start stop = failwith "You need to implement this function."
+let days = [ "first"; "second"; "third"; "fourth"; "fifth"; "sixth"
+             "seventh"; "eighth"; "ninth"; "tenth"; "eleventh"; "twelfth" ]
+
+let gifts = [
+    "a Partridge in a Pear Tree"
+    "two Turtle Doves"
+    "three French Hens"
+    "four Calling Birds"
+    "five Gold Rings"
+    "six Geese-a-Laying"
+    "seven Swans-a-Swimming"
+    "eight Maids-a-Milking"
+    "nine Ladies Dancing"
+    "ten Lords-a-Leaping"
+    "eleven Pipers Piping"
+    "twelve Drummers Drumming"
+]
+
+let line idx =
+    match idx with
+    | 1 -> $"{gifts.[0]}"
+    | idx -> 
+        [1 .. idx-1]
+        |> List.map (fun x -> gifts.[x])
+        |> List.reduce (fun acc gift -> $"{gift}, {acc}")
+        |> (fun x -> $"{x}, and {gifts.[0]}")
+    |> sprintf "On the %s day of Christmas my true love gave to me: %s." days.[idx-1]
+
+let recite start stop =
+    [start..stop]
+    |> List.map line
