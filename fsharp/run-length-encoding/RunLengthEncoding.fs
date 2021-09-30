a68e0fa511e702d0e8ca55c75795c876e90d2495
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2021-09-29 21:32:40 -0400

    [fsharp] run-length-encoding: second submission

--- fsharp/run-length-encoding/RunLengthEncoding.fs
+++ fsharp/run-length-encoding/RunLengthEncoding.fs
@@ -1,41 +1,20 @@
 ﻿module RunLengthEncoding
 
 open System
+open System.Text.RegularExpressions
 
-let private encodeLetter (input: string) =
-    let matchingCharCount = 
-        input
-        |> Seq.takeWhile((=) input.[0])
-        |> Seq.length
+let encode (input: string) =
+    Regex.Matches(input, "(.)\1*")
+    |> Seq.map (fun x ->
+        match x.Value.Length with
+        | 1 -> x.Groups.[1].Value
+        | count -> $"{count}{x.Groups.[1].Value}")
+    |> String.Concat
 
-    match matchingCharCount with
-    | 1 -> (input.[0].ToString(), input.[1..])
-    | x -> ($"{x}{input.[0]}", input.[x..])
-
-let private decodeLetter (input: string) =
-    let numberCharCount = input |> Seq.takeWhile Char.IsDigit |> Seq.length
-    match numberCharCount with
-    | 0 -> (input.[0].ToString(), input.[1..])
-    | x ->
-        let count = input.Substring(0, x) |> int
-        let letter =
-            seq {1..count}
-            |> Seq.map (fun _ -> input.[x])
-            |> String.Concat
-        (letter, input.[x+1..])
-
-let rec encode (input: string) =
-    if input.Length = 0
-    then ""
-    else
-        match encodeLetter input with
-        | letter, "" -> letter
-        | letter, remainder -> letter + encode remainder
-
-let rec decode (input: string) =
-    if input.Length = 0
-    then ""
-    else
-        match decodeLetter input with
-        | letter, "" -> letter
-        | letter, remainder -> letter + decode remainder
+let decode (input: string) =
+    Regex.Matches(input, "(\d*)(.)")
+    |> Seq.map (fun x ->
+        match x.Groups.[1].Value with
+        | "" -> x.Groups.[2].Value
+        | num -> Seq.init (int num) (fun _ -> x.Groups.[2].Value) |> String.Concat)
+    |> String.Concat
