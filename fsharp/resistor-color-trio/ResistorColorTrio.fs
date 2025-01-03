f0a145a57385ffbbeeea0992d1b4eccc8a839053
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2025-01-02 21:29:03 -0500

    [fsharp] Resistor Color Trio - iteration 2

--- fsharp/resistor-color-trio/ResistorColorTrio.fs
+++ fsharp/resistor-color-trio/ResistorColorTrio.fs
@@ -1,22 +1,24 @@
 module ResistorColorTrio
 
 let toNumber = function
-  | "black" -> 0L
-  | "brown" -> 1L
-  | "red" -> 2L
-  | "orange" -> 3L
-  | "yellow" -> 4L
-  | "green" -> 5L
-  | "blue" -> 6L
-  | "violet" -> 7L
-  | "grey" -> 8L
-  | "white" -> 9L
+  | "black" -> 0
+  | "brown" -> 1
+  | "red" -> 2
+  | "orange" -> 3
+  | "yellow" -> 4
+  | "green" -> 5
+  | "blue" -> 6
+  | "violet" -> 7
+  | "grey" -> 8
+  | "white" -> 9
   | _ -> failwith "unknown color"
 
 let label (colors: string list) =
-    let number = (10L * (toNumber colors[0]) + (toNumber colors[1])) * (pown 10L (toNumber colors[2] |> int))
+    let root = 10 * (toNumber colors[0]) + (toNumber colors[1])
+    let zeros = pown 10 (toNumber colors[2])
+    let number = uint64 root * uint64 zeros
 
-    if number >= 1_000_000_000L then $"{number / 1_000_000_000L } gigaohms"
-    elif number >= 1_000_000L then $"{number / 1_000_000L } megaohms"
-    elif number >= 1_000L then $"{number / 1_000L } kiloohms"
+    if number >= 1_000_000_000UL then $"{number / 1_000_000_000UL } gigaohms"
+    elif number >= 1_000_000UL then $"{number / 1_000_000UL } megaohms"
+    elif number >= 1_000UL then $"{number / 1_000UL } kiloohms"
     else $"{number} ohms"
