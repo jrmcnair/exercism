8ea8e259b566bfa88d835f93f862d45175921fd4
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2026-03-29 23:42:26 -0400

    RolePlayingGame: Solution 2

--- fsharp/role-playing-game/RolePlayingGame.fs
+++ fsharp/role-playing-game/RolePlayingGame.fs
@@ -8,9 +8,7 @@ type Player = {
 }
 
 let introduce (player: Player): string =
-    match player.Name with
-    | Some name -> name
-    | None -> "Mighty Magician"
+    Option.defaultValue "Mighty Magician" player.Name
 
 let revive (player: Player): Player option =
     match player.Health with
@@ -19,11 +17,7 @@ let revive (player: Player): Player option =
     | _ -> None
 
 let castSpell (manaCost: int) (player: Player): Player * int =
-    let lowerHealth =
-        if manaCost > player.Health then 0
-        else player.Health - manaCost
-
     match player.Mana with
     | Some mana when mana >= manaCost -> { player with Mana = Some (mana - manaCost) }, manaCost * 2
     | Some _ -> player, 0
-    | None -> { player with Health = lowerHealth }, 0
+    | None -> { player with Health = max (player.Health - manaCost) 0 }, 0
