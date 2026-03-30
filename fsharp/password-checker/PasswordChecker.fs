d0686cfa3d7b185c8ee4b024a686f2e8cff3e01f
Jamie McNair <8563840+jrmcnair@users.noreply.github.com>
2026-03-30 18:21:42 -0400

    [FSharp] Password Checker: first submission

--- fsharp/password-checker/PasswordChecker.fs
+++ fsharp/password-checker/PasswordChecker.fs
@@ -7,12 +7,29 @@ type PasswordError =
     | MissingDigit
     | MissingSymbol
 
-/// Validate the given password against the rules defined in the instructions. If it meets all
-/// of the rules, return a result indicating success; otherwise return a result indicating
-/// failure and an error indicating which rule was violated.
-let checkPassword (password: string) : Result<string, PasswordError> =
-    failwith "Please implement this function"
+let checkLength (password: string) =
+    if String.length password < 12
+    then Result.Error LessThan12Characters
+    else Ok password
+
+let checkCharacters (chars: char seq) (error: PasswordError) (password:string) =
+    if chars |> Seq.exists password.Contains
+    then Ok password
+    else Error error
+
+let checkPassword (password:string) : Result<string, PasswordError> =
+    password |> checkLength
+    |> Result.bind (checkCharacters (seq {'A' .. 'Z'}) MissingUppercaseLetter)
+    |> Result.bind (checkCharacters (seq {'a' .. 'z'}) MissingLowercaseLetter)
+    |> Result.bind (checkCharacters (seq {'0' .. '9'}) MissingDigit)
+    |> Result.bind (checkCharacters "!@#$%^&*" MissingSymbol) 
 
 /// Return a human-readable message indicating the meaning of the given result value.
 let getStatusMessage (result: Result<string, PasswordError>) : string =
-    failwith "Please implement this function"
+    match result with
+    | Ok _ -> "OK"
+    | Error LessThan12Characters -> "Error: does not have at least 12 characters"
+    | Error MissingUppercaseLetter -> "Error: does not have at least one uppercase letter"
+    | Error MissingLowercaseLetter -> "Error: does not have at least one lowercase letter"
+    | Error MissingDigit -> "Error: does not have at least one digit"
+    | Error MissingSymbol -> "Error: does not have at least one symbol"
